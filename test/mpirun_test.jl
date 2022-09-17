4d04b93b9ddbd078030d5b7178aa1632cd4e3a7b
SotaYoshida <syoshida@cc.utsunomiya-u.ac.jp>
2022-09-17 13:24:09 +0900

    test added

---
+++ test/mpirun_test.jl
@@ -0,0 +1,8 @@
+using NuclearToolkit
+using Test
+
+@testset "NuclearToolkit.jl" begin
+    @testset "mpirun" begin
+        @test make_chiEFTint(;nucs=["He4"],itnum=10,optimizer="MCMC",MPIcomm=true)
+    end
+end

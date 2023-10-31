9ef407c0c45148f5c862741c3b9d58731ace1ba4
SotaYoshida <syoshida@cc.utsunomiya-u.ac.jp>
2023-10-31 19:07:05 +0900

    fixed make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,27 +5,30 @@ using NuclearToolkit
 
 DocMeta.setdocmeta!(NuclearToolkit, :DocTestSetup, :(using NuclearToolkit); recursive=true)
 makedocs(;
-        modules=[NuclearToolkit],
-        authors="SotaYoshida <syoshida@cc.utsunomiya-u.ac.jp>",
-        repo="https://github.com/SotaYoshida/NuclearToolkit.jl/blob/{commit}{path}#{line}",
-        sitename="NuclearToolkit.jl",
-        checkdocs=:none,
-        format=Documenter.HTML(;
-            prettyurls=get(ENV, "CI", "false") == "true",
-            canonical="https://SotaYoshida.github.io/NuclearToolkit.jl",
-            assets=String[],
-        ),
-    pages=[
-        "Home" => "index.md",
-        "HowToUse" => "howtouse.md",
-        "FileFormat" => "fileformat.md",
-        "Optional parameters" => "parameters.md",
-        "Contributing to NuclearToolkit" => "contributing.md",
-        "References" => ["ChiEFTint" => "ChiEFTint.md","HartreeFock" => "HartreeFock.md","IMSRG" => "IMSRG.md","ShellModel" => "ShellModel.md"]
+         modules=[NuclearToolkit],
+         authors="SotaYoshida <syoshida@cc.utsunomiya-u.ac.jp>",
+         repo="https://github.com/SotaYoshida/NuclearToolkit.jl/blob/{commit}{path}#{line}",
+         sitename="NuclearToolkit.jl",
+         format=Documenter.HTML(;
+                                prettyurls=get(ENV, "CI", "false") == "true",
+                                canonical="https://SotaYoshida.github.io/NuclearToolkit.jl",
+                                assets=String[],
+                                ),
+         warnonly = [:missing_docs],
+         pages=[
+             "Home" => "index.md",
+             "HowToUse" => "howtouse.md",
+             "FileFormat" => "fileformat.md",
+             "Optional parameters" => "parameters.md",
+             "Contributing to NuclearToolkit" => "contributing.md",
+             "References" => ["ChiEFTint" => "ChiEFTint.md",
+                              "HartreeFock" => "HartreeFock.md",
+                              "IMSRG" => "IMSRG.md",
+                              "ShellModel" => "ShellModel.md"]
     ],
 )
 
 deploydocs(;
-    repo="github.com/SotaYoshida/NuclearToolkit.jl",
-    devbranch="main",
+    repo="github.com/SotaYoshida/NuclearToolkit.jl.git",
+    devbranch="dev",
 )

97a99eecf8e7d61971e96fb5b4ec3016f6c57b29
SotaYoshida <syoshida@cc.utsunomiya-u.ac.jp>
2024-01-05 12:15:30 +0900

    minor update

--- src/chiEFTint/main_chiEFTint.jl
+++ src/chiEFTint/main_chiEFTint.jl
@@ -47,6 +47,7 @@ function make_chiEFTint(;is_show=false,itnum=1,writesnt=true,nucs=[],optimizer="
     end
     if io != stdout; close(io);end
     show_TimerOutput_results(to;tf=is_show)
+    
     return true
 end
 
